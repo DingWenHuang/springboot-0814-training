dd99ad4a4861384b38d436aea9ed9b3f8623908e
黃鼎文 <aa6413779@hotmail.com.tw>
2023-08-14 11:43:41 +0800

    initial setting

---
+++ src/main/java/com/example/springboot0814training/Springboot0814TrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot0814training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0814TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0814TrainingApplication.class, args);
+	}
+
+}
